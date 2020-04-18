4330f84df719fae7e4232839c1a8a6c15be60a1b
xdh <247105782@qq.com>
2020-04-18 20:17:36 +0800

    注册表单的双向数据绑定

---
+++ components/user/registerForm.vue
@@ -0,0 +1,153 @@
+<template>
+    <el-form 
+        :model="form" 
+        ref="form" 
+        :rules="rules" 
+        class="form">
+            <el-form-item class="form-item" prop="username">
+                <el-input 
+                v-model="form.username"
+                placeholder="用户名手机">
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="captcha">
+                <el-input 
+                v-model="form.captcha"
+                placeholder="验证码" >
+                    <template slot="append">
+                        <el-button @click="handleSendCaptcha">
+                            发送验证码
+                        </el-button>
+                    </template>
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="nickname">
+                <el-input 
+                v-model="form.nickname"
+                placeholder="你的名字">
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="password">
+                <el-input 
+                v-model="form.password"
+                placeholder="密码" 
+                type="password"
+                ></el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="checkPassword">
+                <el-input 
+                v-model="form.checkPassword"
+                placeholder="确认密码" 
+                type="password">
+                </el-input>
+            </el-form-item>
+
+            <el-button 
+            class="submit" 
+            type="primary" 
+            @click="handleRegSubmit">
+                注册
+            </el-button>
+        </el-form>
+</template>
+
+<script>
+export default {
+    data(){
+        // 确认密码输入框失去焦点时候触发的验证函数
+        // rule：表示当前的规则，这个不需要使用
+        // value：是输入框的值
+        // callback是必须要调用，可以传入Error对象，实现报错。（new Error是javascript原生的方法）
+        const validatePassword = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.form.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+            // 正常通过
+          callback();
+        }
+      };
+
+
+        return {
+            // 表单数据
+            form: {
+                username: "",       // 手机号码
+                nickname: "",       // 昵称
+                captcha: "",        // 手机验证码
+                password: "",       // 密码
+                checkPassword: ""   // 确认密码
+            },
+            // 表单规则
+            rules: {
+                username: [
+                    { required: true, message: '请输入手机号码', trigger: 'blur' },
+                    { min: 11, max: 11, message: '手机号码格式错误', trigger: 'blur' }
+                ],
+                nickname: [
+                    { required: true, message: "请输入昵称", trigger: "blur" },
+                    {
+                        min: 2,
+                        max: 6,
+                        message: "长度在2到6位字符",
+                        trigger: "blur"
+                    }
+                ],
+                captcha: [
+                    {
+                        required: true,
+                        message: "请输入手机验证码",
+                        trigger: "blur"
+                    }
+                ],
+                password: [
+                    { required: true, message: "请输入密码", trigger: "blur" },
+                    { min: 6, max: 12, message: '密码长度为6-12位', trigger: 'blur' }
+                ],
+                // validator自定义验证，validatePassword是验证的函数
+                checkPassword: [{ validator: validatePassword, trigger: "blur" }]
+            },
+            
+        }
+    },
+    methods: {
+        // 发送验证码
+        handleSendCaptcha(){
+
+        },
+
+
+        // 注册
+        handleRegSubmit(){
+           console.log(this.form)
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .form{
+        padding:25px;
+    }
+
+    .form-item{
+        margin-bottom:20px;
+    }
+
+    .form-text{
+        font-size:12px;
+        color:#409EFF;
+        text-align: right;
+        line-height: 1;
+    }
+
+    .submit{
+        width:100%;
+        margin-top:10px;
+    }
+</style>
