d1db57ad7c1b759e9d42fdbe7631fc5c6d2eb613
xdh <247105782@qq.com>
2020-04-18 17:43:26 +0800

    把登录的方法提取到vuex的actions中

--- store/user.js
+++ store/user.js
@@ -16,5 +16,27 @@ export const mutations = {
     }
 }
 
-
-
+// 重点：actions可以用来存放一些公共的方法，多个组件或者项目通用的方法
+// 顺带的：并且可以调用mutations来修改state
+export const actions = {
+    // 声明一个函数，函数名可以随便起
+    // 该函数有两个参数，第一个参数是Store对象, 第二个参数是传入的参数(不固定)
+    login(store,data){
+        // console.log(this);  打印this  就是store 下面 有个axios 所以可以直接用axios
+        // 请求登录接口
+        this.$axios({
+            url: "/accounts/login",
+            method: "POST",
+            data: data
+        }).then(res => {
+            // console.log(res);
+            const {data} = res;
+            // 这个写法是错的
+            // this.$store.state.user.userInfo = data;
+            
+             // 通过store.commit调用mutations的方法
+            // 由于是在同一个模块下，可以省略前面的user名字
+            store.commit('user/setUserInfo', data);
+        })
+    }
+}
