e5f973a60dca2cc8c92c132deee6153753f32b74
xdh <247105782@qq.com>
2020-04-20 18:21:44 +0800

    通过下拉列表的选中事件获取城市编码，默认选中城市下拉列表的第一个城市，获取出发时间的日期，禁用当前之前的日期选择

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -24,6 +24,7 @@
                 @select="handleDepartSelect"
                 class="el-autocomplete"
                 v-model="form.departCity"
+                @blur="handleDepartBlur"
                 ></el-autocomplete>
             </el-form-item>
 
@@ -33,14 +34,20 @@
                 placeholder="请搜索到达城市"
                 @select="handleDestSelect"
                 class="el-autocomplete"
+                v-model="form.destCity"
+                @blur="handleDestBlur"
                 ></el-autocomplete>
             </el-form-item>
 
             <el-form-item label="出发时间">
                 <!-- change 用户确认选择日期时触发 -->
+                 <!-- value-format 设置时间的格式 -->
                 <el-date-picker type="date" 
                 placeholder="请选择日期" 
                 style="width: 100%;"
+                v-model="form.departDate"
+                :picker-options="pickerOptions"
+                value-format="yyyy-MM-dd"
                 @change="handleDate">
                 </el-date-picker>
             </el-form-item>
@@ -71,7 +78,21 @@ export default {
             ],
             currentTab: 0,
             form:{
-                departCity:""
+                departCity:"",//出发城市
+                departCode:"",//出发城市的字母代码
+                destCity:"",//到达城市
+                destCode:"",//到达城市的字母代码
+                departDate:"" //出发日期
+            },
+            // 出发城市下拉列表的数据
+            departCities:[],
+            // 到达城市下拉列表的数据
+            destCities:[],
+            //日期禁用选项  禁用昨天之前的日期
+            pickerOptions: {
+                disabledDate(time) {
+                return time.getTime() < Date.now() - 3600 * 1000 * 24;
+                }
             },
         }
     },
@@ -81,7 +102,7 @@ export default {
             
         },
         
-        // 监听出发城市输入框的变化，一旦输入框的文字发生了变化，就会触发该这个事件
+        // 监听  出发城市  输入框的变化，一旦输入框的文字发生了变化，就会触发该这个事件
         // value是输入框的值 cb是函数必须要调用的
         //cb接收的参数有个固定的格式，参数必须是一个数组，且数组里面每一项都必须是 对象，每个对象必须要有value
         // 这里这个插件版本有问题，手动把element-ui的版本回退到2.12.0    执行命会覆盖  npm  install element-ui@2.12.0
@@ -103,42 +124,69 @@ export default {
                     v.value = v.name.replace('市','');
                     return v;
                 });
+                // 保存到data中，给blur事件使用，失去焦点选择第一个
+                this.departCities = newData;
+                // cb是函数必须要调用的
                 cb(newData);
             })
         },
-        // 出发城市输入框获得焦点时触发
-        // value 是选中的值，cb是回调函数，接收要展示的列表
-        // queryDepartSearch(value, cb){
-        //     cb([
-        //         {value: 1},
-        //         {value: 2},
-        //         {value: 3},
-        //     ]);
-        // },
+        
+        // 出发城市输入框失去焦点事件
+        handleDepartBlur(){
+            if(this.departCities.length>0){
+                this.form.departCity = this.departCities[0].value;
+                this.form.departCode = this.departCities[0].sort;
+            }
+        },
 
-        // 目标城市输入框获得焦点时触发
+        //  目标城市  输入框获得焦点时触发
         // value 是选中的值，cb是回调函数，接收要展示的列表
         queryDestSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
+             if(!value){return}
+            // 请求和value相关的文字
+            this.$axios({
+                url:"/airs/city",
+                params:{
+                    name:value
+                }
+            }).then(res=>{
+                // console.log(res);
+                const {data} = res.data;
+                // 因为cb里面的每个对象里面必须要有value 所以要map添加
+                // map必须要retur；！！！！！！！
+                const newData = data.map(v=>{
+                    v.value = v.name.replace('市','');
+                    return v;
+                });
+                // 保存到data中，给blur事件使用，失去焦点选择第一个
+                this.destCities = newData;
+                // cb是函数必须要调用的
+                cb(newData);
+            })
         },
-       
+       // 到达城市 输入框失去焦点事件
+        handleDestBlur(){
+            if(this.departCities.length>0){
+                this.form.destCity = this.destCities[0].value;
+                this.form.destCode = this.destCities[0].sort;
+            }
+        },
+
+
         // 出发城市下拉选择时触发
         handleDepartSelect(item) {
-            
+            // console.log(item);
+            this.form.departCode = item.sort
         },
 
         // 目标城市下拉选择时触发
         handleDestSelect(item) {
-            
+            this.form.destCode = item.sort
         },
 
         // 确认选择日期时触发
         handleDate(value){
-           
+        //    console.log(value);
         },
 
         // 触发和目标城市切换时触发
